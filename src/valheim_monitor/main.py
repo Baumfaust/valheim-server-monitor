d8b74afd5934ab983312d8a5301b2517d22a6123
Baumfaust <baumfaust@gmx.com>
2025-02-16 10:56:01 +0100

    removed file log handler

--- src/valheim_monitor/main.py
+++ src/valheim_monitor/main.py
@@ -15,7 +15,6 @@ logging.basicConfig(
     level=log_level,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("../../app.log"),
         logging.StreamHandler()
     ]
 )
